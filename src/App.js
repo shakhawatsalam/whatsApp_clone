34f824d9e3d99e0cc103f785ec7ae27ff230a99d
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-06-05 11:33:16 +0600

    work is in progress 31:00

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
-
       <div className='app_body'>
         {/* Sidebar  */}
-        Hello world
+    
         {/* Chat  */}
       </div>
     </div>
