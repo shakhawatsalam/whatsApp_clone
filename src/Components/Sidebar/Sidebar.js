e518735d4cd4aa48e214c0b6e226dbb52648c152
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-06-07 12:35:45 +0600

    some button is added

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Sidebar.css'
 import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import ChatIcon from '@mui/icons-material/Chat';
 import { MoreVert } from '@mui/icons-material';
@@ -10,9 +11,17 @@ const Sidebar = () => {
             <div className="sidebar_header">
                 <Avatar></Avatar>
                 <div className='sidebar__headerRight'>
-                    <DonutLargeIcon />
-                    <ChatIcon />
-                    <MoreVert></MoreVert>
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert></MoreVert>
+                    </IconButton>
+
+
                 </div>
             </div>
             <div className='sidebar_search'>
